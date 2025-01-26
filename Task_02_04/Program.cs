73ad5aa5db700dac938a01bca975730e622f0f4e
nagaevdanil3422 <danilnagaev78@gmail.com>
2025-01-26 17:10:13 +0500

    Решено задача 4 урока 3

---
+++ Task_02_04/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_02_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Пользователь вводит свою дату рождения построчно (сначала год, потом месяц и в конце дату) произведите
+расчет является ли пользователь совершеннолетним на текущую дату и выведите соответствующее сообщение об
+этом
+             */
+            Console.WriteLine("Введите текущий год: ");
+            int user;
+            user = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите сегодняшний месяц: ");
+
+            int user1;
+            user1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите сегодняшнее число: ");
+            int user2;
+            user2 = int.Parse(Console.ReadLine());
+
+
+            
+        }
+    }
+}
