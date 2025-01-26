d72fe0e9af2952c2c30862cebdc31e5a2d07949c
nagaevdanil3422 <danilnagaev78@gmail.com>
2025-01-26 21:44:05 +0500

    Задача 10 из урока 4 решена

--- Task_04_09/Program.cs
+++ Task_04_09/Program.cs
@@ -4,19 +4,19 @@
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 1, 2, 3, 2, 4, 5, 6, 1, 7, 8, 6, 9, 10, 10 }; 
+            int[] numbers = { 1, 2, 3, 2, 4, 5, 6, 1, 7, 8, 6, 9, 10, 10 };
             int size = numbers.Length;
 
-            
-            int[] counts = new int[101]; 
 
-            
+            int[] counts = new int[101];
+
+
             for (int i = 0; i < size; i++)
             {
                 counts[numbers[i]]++;
             }
 
-            
+
             Console.WriteLine("Уникальные элементы:");
             for (int i = 0; i < counts.Length; i++)
             {
@@ -24,5 +24,7 @@
                 {
                     Console.Write(i + " ");
                 }
+            }
+        }
     }
 }
