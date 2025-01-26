8d7cf3e1e9772b0180b756f7a8e861cb22da1bed
nagaevdanil3422 <danilnagaev78@gmail.com>
2025-01-26 15:38:22 +0500

    Выполнена 2 задача 2 урока

---
+++ Task_02_02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Channels;
+
+namespace Task_02_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 8;
+            int b = 14;
+            double c = Math.PI / 4;
+            double z = Math.Abs(a - b);
+            double x = Math.Pow(Math.Sin(c), 2) + Math.Tan(c);
+            double v = z * x;
+            
+            double K = Math.Pow(b + Math.Pow(a - 1, 1.0 / 3.0), 1.0 / 4.0);
+            double o = K / v;
+            Console.WriteLine(o);
+  
+        }
+    }
+}
