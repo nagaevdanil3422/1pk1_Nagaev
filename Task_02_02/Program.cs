e846982f5a671f9e6d9628da35414f166a9dad61
nagaevdanila3422 <danilnagaev78@gmail.com>
2025-01-28 22:00:42 +0500

    решена задача 2 из урока 2

--- Task_02_02/Program.cs
+++ Task_02_02/Program.cs
@@ -8,13 +8,13 @@ namespace Task_02_02
         {
             int a = 8;
             int b = 14;
-            double c = Math.PI / 4;
+            double sum = Math.PI / 4;
             double z = Math.Abs(a - b);
-            double x = Math.Pow(Math.Sin(c), 2) + Math.Tan(c);
-            double v = z * x;
+            double x = Math.Pow(Math.Sin(sum), 2) + Math.Tan(sum);
+            double vo = z * x;
             
-            double K = Math.Pow(b + Math.Pow(a - 1, 1.0 / 3.0), 1.0 / 4.0);
-            double o = K / v;
+            double us = Math.Pow(b + Math.Pow(a - 1, 1.0 / 3.0), 1.0 / 4.0);
+            double o = us / vo;
             Console.WriteLine(o);
   
         }
