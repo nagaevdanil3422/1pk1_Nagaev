ef9007dfa8587de076e96928648c76e59147602a
nagaevdanila3422 <danilnagaev78@gmail.com>
2025-01-28 22:27:08 +0500

    Решена задача 8 из урока 4

--- Task_04_08/Program.cs
+++ Task_04_08/Program.cs
@@ -7,32 +7,28 @@
             /*  Дан массив, содержащий последовательность 50 случайных чисел. Найти количество пар элементов, значения
                 которых равны.
             */
-            Random random = new Random();
-            int[] rast = new int[50];
-
+            Random rnd = new Random();
+            int[] ra = new int[50];
             for (int i = 0; i < 50; i++)
             {
 
-                rast[i] = random.Next(1, 11);
+                ra[i] = rnd.Next(1, 11);
             }
-
             Dictionary<int, int> counts = new Dictionary<int, int>();
-            foreach (var number in rast)
+            foreach (int number in ra)
             {
                 counts[number] = counts.GetValueOrDefault(number, 0) + 1;
             }
-
-            int pairsCount = 0;
-            foreach (var count in counts.Values)
+            int pk = 0;
+            foreach (int count in counts.Values)
             {
                 if (count > 1)
                 {
-                    pairsCount += count * (count - 1) / 2; 
+                    pk += count * (count - 1) / 2; 
                 }
             }
-
+            Console.WriteLine($"Количество пар элементов, которыe равны: {pk}");
             
-            Console.WriteLine($"Количество пар элементов, значения которых равны: {pairsCount}");
 
 
 
