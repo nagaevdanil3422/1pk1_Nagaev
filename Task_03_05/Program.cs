799607382f01923685d3283c58ac25a55ddf2b08
nagaevdanila3422 <danilnagaev78@gmail.com>
2025-01-28 22:07:57 +0500

    решена задача 5 из урока 3

--- Task_03_05/Program.cs
+++ Task_03_05/Program.cs
@@ -10,20 +10,20 @@
               время работы программы
              */
             Console.Write("Введите начальную температуру в градусах Цельсия: ");
-            double i = Convert.ToDouble(Console.ReadLine());
+            double user = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("Введите конечную температуру в градусах Цельсия: ");
-            double er = Convert.ToDouble(Console.ReadLine());
+            double user1 = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("Введите шаг изменения температуры в градусах Цельсия: ");
-            double w = Convert.ToDouble(Console.ReadLine());
+            double user2 = Convert.ToDouble(Console.ReadLine());
 
-            // Вывод заголовка таблицы
-            Console.WriteLine("\nТемпература (°C) | Температура (°F)");
+            
+            Console.WriteLine("\n      (°C)         |       (°F)");
             Console.WriteLine("-------------------|-------------------");
 
-            // Цикл для вычисления и вывода значений
-            for (double celsius = i; celsius <= er; celsius += w)
+            
+            for (double celsius = user; celsius <= user1; celsius += user2)
             {
                 double f = celsius * 1.8 + 32;
                 Console.WriteLine($"{celsius,-18:F2} | {f,-18:F2}");
