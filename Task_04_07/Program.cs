ba2385844d8cbce30ce1b579e8062d1d8afbba1c
nagaevdanila3422 <danilnagaev78@gmail.com>
2025-01-28 22:24:08 +0500

    решена задача 7 из урока 4

--- Task_04_07/Program.cs
+++ Task_04_07/Program.cs
@@ -10,43 +10,42 @@ namespace Task_04_07
             /*   В массиве на 30 элементов содержатся данные по росту учеников в классе. Рост мальчиков условно задан
                  отрицательными значениями. Вычислить и вывести количество мальчиков и девочек в классе и средний рост для
                  мальчиков и девочек. При выводе избавиться от отрицательных значений.
-
             */
 
-            Random random = new Random();
-            int[] rost = new int[30];
+            Random rnd = new Random();
+            int[] ri = new int[30];
 
             for (int i = 0; i < 30; i++)
             {
 
-                rost[i] = random.Next(-180, 201);
+                ri[i] = rnd.Next(-180, 201);
             }
-            int boysCount = 0;
-            int girlsCount = 0;
-            int boysSum = 0;
-            int girlsSum = 0;
+            int b = 0;
+            int g = 0;
+            int by = 0;
+            int gi = 0;
 
-            foreach (var height in rost)
+            foreach (var height in ri)
             {
-                if (height < 0) // Мальчики
+                if (height < 0) 
                 {
-                    boysCount++;
-                    boysSum += Math.Abs(height);
+                    b++;
+                    by += Math.Abs(height);
                 }
-                else // Девочки
+                else 
                 {
-                    girlsCount++;
-                    girlsSum += height;
+                    g++;
+                    gi += height;
                 }
 
             }
-            double averageBoysHeight = boysCount > 0 ? (double)boysSum / boysCount : 0;
-            double averageGirlsHeight = girlsCount > 0 ? (double)girlsSum / girlsCount : 0;
+            double ert = b > 0 ? (double)by / b : 0;
+            double erty = g > 0 ? (double)gi / g : 0;
 
-            Console.WriteLine($"Количество мальчиков: {boysCount}");
-            Console.WriteLine($"Количество девочек: {girlsCount}");
-            Console.WriteLine($"Средний рост мальчиков: {averageBoysHeight:F2} см");
-            Console.WriteLine($"Средний рост девочек: {averageGirlsHeight:F2} см");
+            Console.WriteLine($"Количество мальчиков: {b}");
+            Console.WriteLine($"Количество девочек: {g}");
+            Console.WriteLine($"Средний рост мальчиков: {ert:F2} см");
+            Console.WriteLine($"Средний рост девочек: {erty:F2} см");
         }
 
     }
