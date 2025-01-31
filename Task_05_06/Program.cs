5de8086aabb214772701be991079273dd293d227
nagaevdanil3422 <danilnagaev78@gmail.com>
2025-01-31 23:17:50 +0500

    Решена не до конца

--- Task_05_06/Program.cs
+++ Task_05_06/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Task_05_06
+﻿using System.Net;
+using System.Security.Cryptography;
+
+namespace Task_05_06
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[,] masiv = new int[10, 5];
+
+            for (int i = 0; i < masiv.GetLength(0); i++)
+            {
+                for (int j = 0; j < masiv.GetLength(1); j++)
+                {
+                    masiv[i, 0] = 0;
+                    masiv[i, 1] = (i + 1) * 2;
+                    masiv[i, 2] = (i + 1) * 3;
+                    masiv[i, 3] = (i + 1) * 4;
+                    masiv[i, 4] = (i + 1) * 5;
+                    
+                    Console.Write(masiv[i, j] + "\t");
+                    int[,] you = TransposeArray(masiv[i, j]);
+                    Console.Write(you);
+                }
+                Console.WriteLine();
+                Console.WriteLine("\nОбновленный массив (транспонированный):");
+                
+            }
+            // Решена не до конца
+            
+
         }
     }
 }
