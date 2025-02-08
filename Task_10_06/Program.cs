cd7b72b9a12d9e4ed16699dcfd05f26ec36f2c0c
nagaevdanil3422 <danilnagaev78@gmail.com>
2025-02-08 20:51:07 +0500

    Решена задача 6 из урока 10

---
+++ Task_10_06/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography;
+
+namespace Task_10_06
+{
+    internal class Program
+    {   //Создать Метод ArrayGeneration не возвращающий значения, принимает целое число n, выводит на консоль сгенерированный массив размерности n*n.
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                ArrayGeneration();
+                Console.ReadKey();
+            }
+        }
+        /// <summary>
+        /// Выводит массив размером n*n 
+        /// </summary>
+        public static void ArrayGeneration()
+        {
+            Random random = new Random();
+            Console.WriteLine("Введите размер массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+        }
+        
+                         
+                    
+        
+        
+    }
+}
