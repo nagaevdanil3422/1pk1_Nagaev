a266e5f67e418ae019b373514e94bd878fbe6a52
nagaevdanil3422 <danilnagaev78@gmail.com>
2025-02-15 20:30:23 +0500

    Решена задача 08 из урока 11

---
+++ Task_11_08/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Task_11_08
+{   /*  
+     *      Использование params и out: Напишите метод, который принимает переменное количество
+     *      чисел и возвращает их сумму и максимальное значение через выходные параметры (out)
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sum;
+            int max;
+
+            CalculateSumAndMax( out sum, out max, 1, 2, 3, 4, 5 );
+
+            Console.WriteLine($"Сумма: {sum} \nМаксимальное значение: {max}");
+        }
+
+        public static void CalculateSumAndMax( out int sum, out int max, params int[] numbers)
+        {
+            sum = 0;
+            max = 0;
+
+            foreach (int number in numbers)
+            {
+                sum += number;
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+        }
+    }
+}
