cc0e412628ed12aac9f5f6819c07d91dc0875993
nagaevdanil3422 <danilnagaev78@gmail.com>
2025-02-15 18:51:57 +0500

    Решена задача 1 из урока 11

---
+++ Task_11_01/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_11_01
+{   /*  
+        Передача по значению: Напишите метод, который принимает два целых числа и меняет их местами.
+      Проверьте, изменились ли значения переменных вне метода
+    */
+    internal class Program
+    {
+        static void Main()
+        {
+            int a = 5;
+            int b = 10;
+
+            Console.WriteLine($"До вызова метода: a = {a}, b = {b}");
+            SwapByValue(ref a, ref b);
+            Console.WriteLine($"После вызова метода: a = {a}, b = {b}");
+        }
+
+        static void SwapByValue(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+}
