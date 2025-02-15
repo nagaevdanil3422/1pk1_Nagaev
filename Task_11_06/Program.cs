5c2805407c090b76e04cf033048db4e97b31a0f5
nagaevdanil3422 <danilnagaev78@gmail.com>
2025-02-15 20:15:11 +0500

    Решена задача 06 из урока 11

---
+++ Task_11_06/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_11_06
+{   /*  Передача массива по значению: Напишите метод, который принимает массив целых чисел и
+        изменяет его элементы, увеличивая каждый на 1. Проверьте, изменился ли оригинальный
+        массив вне метода
+    */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1, 2, 3, 4, 5, 6};
+            Console.WriteLine("Оригинальный список: " + string.Join(", ", array));
+            ModifiArray(array);
+            Console.WriteLine("Измененый список: " + string.Join(", ", array));
+
+        }
+        public static void ModifiArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] += 1;
+            }
+        }
+    }
+}
