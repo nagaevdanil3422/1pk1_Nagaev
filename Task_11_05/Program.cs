cc801991074e9ebe8bba32943c337a26f123c3db
nagaevdanil3422 <danilnagaev78@gmail.com>
2025-02-15 19:57:11 +0500

    Решена задача 05 из урока 11

---
+++ Task_11_05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Task_11_05
+{  /*  
+    *  Комбинирование ref и out: Напишите метод, который принимает два числа по ссылке (ref) и
+    *  возвращает их сумму и произведение через выходные параметры (out)
+    */
+    internal class Program
+    {
+        static void Main()
+        {
+            int a = 5;
+            int b = 6;
+            Numbers(ref a, ref b, out int sum, out int proiz);
+            Console.WriteLine($"Сумма чисел: {sum}");
+            Console.WriteLine($"Произведение чисел: {proiz}");
+
+        }
+        public static void Numbers(ref int x, ref int y, out int sum, out int proiz)
+        {
+            sum = x + y;
+            proiz = x * y;
+            
+        }
+        
+    }
+}
