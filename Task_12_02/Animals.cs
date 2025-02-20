3746f2185dae1423431091252d8a025f62f0bcbc
nagaevdanil3422 <danilnagaev78@gmail.com>
2025-02-20 23:39:43 +0500

    Решена задача 02 из урока 12

---
+++ Task_12_02/Animals.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12_02
+{
+    class Animals
+    {
+        public string nameCat;
+        public int ageCat;
+        public string IsHappyCat;
+        public string nameDog;
+        public int ageDog;
+        public string IsHappyDog;
+
+        public void InfoCat()
+        {
+            Console.WriteLine($"Name cat: {nameCat}");
+            Console.WriteLine($"Age cat: {ageCat}");
+            Console.WriteLine($"Is the cat happy: {IsHappyCat}");
+        }
+        public void InfoDog()
+        {
+            Console.WriteLine($"Name dog: {nameDog}");
+            Console.WriteLine($"Age dog: {ageDog}");
+            Console.WriteLine($"Is the dog happy: {IsHappyDog}");
+        }
+    }
+}
---
+++ Task_12_02/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task_12_02
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Animals animal = new Animals();
+            string namecat = animal.nameCat;
+            int agecat = animal.ageCat;
+            string ishappycat = animal.IsHappyCat;
+
+            animal.nameCat = "Barsik";
+            animal.ageCat = 2;
+            animal.IsHappyCat = "Счастлив";
+
+
+            string namedog = animal.nameDog;
+            int agedog = animal.ageDog;
+            string ishappydog = animal.IsHappyDog;
+
+            animal.nameDog = "Bobik";
+            animal.ageDog = 4;
+            animal.IsHappyDog = "Не счастлив";
+
+            animal.InfoCat();
+            Console.WriteLine();
+            animal.InfoDog();
+
+
+        }
+    }
+}
