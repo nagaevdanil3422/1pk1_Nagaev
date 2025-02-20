958830d955f70b3984409aa5acb2428fbd3277a2
nagaevdanil3422 <danilnagaev78@gmail.com>
2025-02-20 21:36:37 +0500

    Решен проект из урока 12

---
+++ Task_12_01/Book.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesDemo.Publications
+{
+#pragma warning disable
+
+    internal class Book
+    {
+        public string title; //поле класса - хранит название книги
+        public string autor; //поле класса - хранит автора книги
+        bool isReading = false; //поле класса - хранит статус книги (выдана ли она читателю)
+
+        //метод класса - печатает в консоль информацию о книге
+        public void GetInfo()
+        {
+            Console.WriteLine($"{autor} : {title}");
+        }
+        //метод класса - выдает книгу читателю и ставит отметку о выдаче
+        public void GetBoot()
+        {
+            if (isReading)
+                Console.WriteLine($"книга {title} в данный момент выдана другому читателю");
+            else
+            {
+                isReading = true;
+                Console.WriteLine("книга выдана на неделю");
+            }
+        }
+
+    }
+    public class Book1
+    {
+        public string Title { get; private set; }
+        public bool IsAvailable { get; private set; }
+
+        public Book1(string title)
+        {
+            Title = title;
+            IsAvailable = true; 
+        }
+
+        public void Borrow()
+        {
+            if (IsAvailable)
+            {
+                IsAvailable = false;
+                Console.WriteLine($"Книга '{Title}' была взята.");
+            }
+            else
+            {
+                Console.WriteLine($"Книга '{Title}' недоступна.");
+            }
+        }
+
+        public void ReturnBook()
+        {
+            if (!IsAvailable)
+            {
+                IsAvailable = true;
+                Console.WriteLine($"Книга '{Title}' была возвращена и теперь доступна.");
+            }
+            else
+            {
+                Console.WriteLine($"Книга '{Title}' уже доступна.");
+            }
+        }
+    }
+}
+
+
---
+++ Task_12_01/Program.cs
@@ -0,0 +1,40 @@
+﻿using ClassesDemo.Publications;
+
+namespace ClassesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            var book2 = new Book();
+            var book3 = new Book() { title = "Автостопом по галактике", autor = "Дуглас Адамс" };
+            Book book4 = new Book() { title = "Хоббит", autor = "Джон Толкин" };
+
+
+            book1.autor = "Рей Бредберри";
+            book1.title = "Марсианские хроники";
+            book1.GetInfo();    //Рей Бредберри : Марсианские хроники
+
+            book2.autor = "Джона Роулинг";
+            book2.title = "Гарри Поттер и философский камень";
+            book2.GetInfo();    //Джона Роулинг : Гарри Поттер и философский камень
+
+            //выдача книги
+            book2.GetBoot(); // книга выдана на неделю
+
+            //попытка выдать уже занятую книгу
+            book2.GetBoot(); // книга Гарри Поттер и философский камень в данный момент выдана другому читателю
+
+             
+                Book1 book = new Book1("Война и мир");
+
+                book.Borrow();
+                book.ReturnBook();            
+        }
+    }
+}
+
+
+
+
