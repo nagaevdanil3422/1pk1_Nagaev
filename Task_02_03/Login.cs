2326dab37647870c3d1577f566678e5bc234a324
nagaevdanil3422 <danilnagaev78@gmail.com>
2025-02-21 19:42:41 +0500

    Решена задача 03 из урока 2

---
+++ Task_02_03/Login.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_02_03
+{
+    class Login
+    {
+        public string login;
+        public string password;
+
+        public void Register()
+        {
+            
+            Console.Write("Ввведите логин: ");
+            string userlog = Console.ReadLine();
+            Console.Write("Введите пароль: ");
+            string userpass = Console.ReadLine();
+        }
+        public void PassNew()
+        {
+            Console.Write("Введите новый пароль: ");
+            string userpass = Console.ReadLine();
+        }
+        public void LogNew()
+        {
+            Console.Write("Ввведите новый логин: ");
+            string userlog = Console.ReadLine();
+        }
+    }
+}
---
+++ Task_02_03/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Task_02_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Login username = new Login();
+            string usernamelog = username.login;
+            string usernamepass = username.password;
+            
+            Console.WriteLine("Был произведен вход в программу");
+            Console.WriteLine();
+            while (true)
+            {
+                Console.WriteLine("Что хотите сделать?\n1. Регистрация\n2. Сменить логин\n3. Сменить пароль\n4. Выйти из программы");
+                Console.Write("Введите цифру действия: ");
+                string us = Console.ReadLine();
+                Console.WriteLine();
+                if (us == "1")
+                {
+                    username.Register();
+                    Console.WriteLine();
+                    Console.WriteLine("Вы зарегестрированы!");
+                    Console.WriteLine("--------------------------------------");
+
+                }
+                else if (us == "2")
+                {
+                    username.LogNew();
+                    Console.WriteLine();
+                    Console.WriteLine("Логин изменен");
+                    Console.WriteLine("--------------------------------------");
+                }
+                else if (us == "3")
+                {
+                    username.PassNew();
+                    Console.WriteLine();
+                    Console.WriteLine("Пароль изменен");
+                    Console.WriteLine("--------------------------------------");
+                }
+                else if (us == "4")
+                {
+                    Console.WriteLine("Thank you!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                    break;
+                }
+                    Console.WriteLine();
+                
+            }
+        }
+    }
+}
