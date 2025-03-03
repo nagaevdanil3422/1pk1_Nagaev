f5a0e3c446aca7674420ba6ff6529e2e3fc33586
nagaevdanil3422 <danilnagaev78@gmail.com>
2025-03-03 23:49:37 +0500

    Не доконца решена задача 02 из урока 13

---
+++ Task_13_02/Animal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13_02
+{
+    class Animal
+    {
+        public string name;
+        public string vidanimals;
+        public int age;
+        public int weight;
+        public string health;
+
+
+        public void InfoAnimals()
+        {
+            Console.Write($"Кличка животного: {name}\nВид животного: {vidanimals}\nВозраст: {age}\nВес: {weight}\nСостояние о здоровья: {health}");
+        }
+
+        
+        
+        public int SetWeight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+    }
+}
---
+++ Task_13_02/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task_13_02
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+            Animal Cat = new Animal();
+            Console.WriteLine($"1. Информация о животном\n2. Изменить данные веса\n3. Изменить состояние здоровья");
+            Console.Write("Введите цифру того что хотите узнать о животном: ");
+            string user = Convert.ToString(Console.ReadLine());
+            string namecat = Cat.name;
+            string vidCat = Cat.vidanimals;
+            int ageCat = Cat.age;
+
+            Console.Write("Введите вес котенка: ");
+            int Weight = Convert.ToInt32(Console.ReadLine());
+            Cat.weight = Weight;
+            int weightCat = Cat.weight;
+
+            namecat = "Tom";
+            vidCat = "Котенок";
+            ageCat = 2;
+
+            Console.Write("Введите здоров ли котенок или нет: ");
+            string Health = Convert.ToString(Console.ReadLine());
+            Cat.health = Health;
+            string healthCat = Cat.health;
+
+            if (user == "1")
+            {
+                Cat.InfoAnimals();
+            }
+            else if (user == "2")
+            {
+                Console.WriteLine(Cat.SetWeight);
+            }
+
+
+            
+        }
+    }
+}
