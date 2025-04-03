8de0f910ebd321a0711cb28c64317d8358a12b0d
nagaevdanil3422 <danilnagaev78@gmail.com>
2025-04-03 22:24:26 +0500

    Решена задача 05 из урока 20

---
+++ Task_20_05/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Task_20_05
+{
+    enum AccessLevel
+    {
+        Guest,
+        User,
+        Moderator,
+        Admin
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+            AccessLevel userAccess = AccessLevel.Moderator;
+
+
+            CheckPermission(userAccess, "delete post");
+        }
+
+        static void CheckPermission(AccessLevel accessLevel, string action)
+        {
+            if (action == "delete post")
+            {
+                if (accessLevel == AccessLevel.Moderator || accessLevel == AccessLevel.Admin)
+                {
+                    Console.WriteLine("Действие выполнено: Пост успешно удален.");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: Недостаточно прав для удаления поста!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: Неизвестное действие!");
+            }
+        }
+    }
+}
+                
+            
+        
+
+ 
