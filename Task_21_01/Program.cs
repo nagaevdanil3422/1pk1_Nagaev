d392d97c6b33b712a8f2aec93a4e552eef82444c
nagaevdanil3422 <danilnagaev78@gmail.com>
2025-04-07 23:22:30 +0500

    Решена задача 1 из урока 21

---
+++ Task_21_01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace Task_21_01
+{
+    /*
+     * Дан список чисел. Создать новый список, содержащий только чётные числа из исходного списка, умноженныена10
+    */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            List<int> NumbersTen = numbers
+             .Where(n => n % 2 == 0)
+             .Select(n => n * 10)
+             .ToList();
+            foreach (int m in NumbersTen)
+            {
+                Console.WriteLine(m);
+            }
+            
+
+            
+
+        }
+    }
+}
