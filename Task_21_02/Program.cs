0dab47f21816f4a135bfc7b0d306bcf7036811c2
nagaevdanil3422 <danilnagaev78@gmail.com>
2025-04-07 23:35:44 +0500

    Решена задача 02 из урока 21

---
+++ Task_21_02/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Task_21_02
+{
+    /*  Дан текст. Написать метод, который возвращает словарь, где ключ — слово, а значение —количество его вхождений в тексте. */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Это пример текста, в котором мы будем считать количество вхождений слов. Слова могут повторяться, и мы хотим знать, сколько раз каждое слово встречается.";
+
+            Dictionary<string, int> wordCount = CountWords(text);
+
+
+            foreach (var pair in wordCount)
+            {
+                Console.WriteLine($"Слово: '{pair.Key}' - Количество: {pair.Value}");
+            }
+        }
+        static Dictionary<string, int> CountWords(string text)
+        {
+
+            char[] znaki = new char[] { ' ', ',', '.', '!', '?', ';', ':' };
+            string[] words = text.ToLower().Split(znaki, StringSplitOptions.RemoveEmptyEntries);
+
+
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (wordCount.ContainsKey(word))
+                {
+                    wordCount[word]++;
+                }
+                else
+                {
+                    wordCount[word] = 1;
+                }
+            }
+
+            return wordCount;
+        }
+    }
+}
