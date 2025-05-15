49bd5d8e0db2a513bc00e2dafe4f384869103487
nagaevdanil3422 <danilnagaev78@gmail.com>
2025-05-16 01:28:52 +0500

    Не закончено

---
+++ Task_38_04/BazaData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Task_38_04
+{
+    internal class BazaData
+    {
+        List<RegistrationStudent> students = new();
+        List<RegistrationStudent> Students => students;
+        private string saveFileName = "Students.json";
+        public void SaveStudents()
+        {
+            
+                
+            
+        }
+    }
+}
---
+++ Task_38_04/RegistrationStudent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_38_04
+{
+    public enum StatusPol
+    {
+        men,
+        women
+    }
+    [Serializable]
+    public class RegistrationStudent
+    {
+        public string SecondNameTextBox { get; set; }
+        public string FirstNameTextBox { get; set; }
+        public string MiddleNameTextBox { get; set; }
+        public StatusPol Pol { get; set; }
+        public int DateBirthday { get; set; }
+
+        public RegistrationStudent(string secondNameTextBox, string firstNameTextBox, string middleNameTextBox, StatusPol pol, int dateBirthday)
+        {
+            SecondNameTextBox = secondNameTextBox;
+            FirstNameTextBox = firstNameTextBox;
+            MiddleNameTextBox = middleNameTextBox;
+            Pol = pol;
+            DateBirthday = dateBirthday;
+        }
+        public override string ToString()
+        {
+            return $"{SecondNameTextBox} {FirstNameTextBox} {MiddleNameTextBox}";
+        }
+        public string StringStatus()
+        {
+            switch (Pol)
+            {
+                case StatusPol.men: return "Мужской"; break;               
+                case StatusPol.women: return "Женский"; break;
+            }
+            return null;
+
+        }
+    }
+}
